4e1cc3066e7b77a36145979cc6b207c4c51de4dc
Jameslambdastudent <63371643+Jameslambdastudent@users.noreply.github.com>
2020-09-04 20:11:24 -0400

    Mvp Finish

--- src/Episodes.test.js
+++ src/Episodes.test.js
@@ -1,9 +1,8 @@
 import React from "react";
 import {render} from "@testing-library/react";
-
 import Episodes from "./components/Episodes";
 
-test("Checks if episodes component renders correctly with and without episode data", ()=>{
+test("Checking to see if episodes component renders correctly with and without episode data", ()=>{
     const {queryAllByTestId, rerender} = render(<Episodes episodes={[]}/>);
 
     expect(queryAllByTestId("episode")).toHaveLength(0);
@@ -13,16 +12,8 @@ test("Checks if episodes component renders correctly with and without episode da
             id: '1',
             name: "Episode 1"
         },
-        {
-            id: '2',
-            name: "Episode 2"
-        },
-        {
-            id: '3',
-            name: "Episode 3"
-        }
-    ];
+    ]
 
     rerender(<Episodes episodes={mockData}/>);
-    expect(queryAllByTestId("episode")).toHaveLength(3);
+    expect(queryAllByTestId("episode")).toHaveLength(1);
 });
